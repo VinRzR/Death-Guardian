8cffa4a4468400681f6dd326d7d3a1eff0d71af9
Vinicius Rezende <42686498+VinRzR@users.noreply.github.com>
2022-05-21 16:02:09 -0300

    movimentação

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+    // Update is called once per frame
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
